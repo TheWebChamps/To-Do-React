30a23d65a4309c7c053b82c080ebe4c5f1c145b3
Shaurya Aher <shauryaaher1200@gmail.com>
2022-02-16 17:19:15 +0530

    Test 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+     bin: 0
+    };
+  }
+  tickyTocky() {
+    this.setState(() => ({
+      bin: this.state.bin + 1
+    }));
+  }
+  componentDidMount() {
+    setInterval(() => {this.tickyTocky()}, 1000);
+  }
+  render() {
+    return <center><h1>{this.state.bin}</h1></center>;
+  }
 }
-
-export default App;
