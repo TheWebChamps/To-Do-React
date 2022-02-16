f15d33ecfd360cf539f1776c156e7dd553cc8ac9
Shaurya Aher <shauryaaher1200@gmail.com>
2022-02-16 22:30:20 +0530

    Speedy performance improvements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import { Component } from "react";
 import "./index.css";
 
-export default class App extends React.Component {
+export default class App extends Component {
   constructor() {
     super();
     this.state = {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App.js';
