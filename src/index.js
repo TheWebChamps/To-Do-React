15d16dd729471446b1b49e3b8d525f844d4b92fd
Shaurya Aher <shauryaaher1200@gmail.com>
2022-02-17 12:29:22 +0530

    Third test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import { render } from 'react-dom';
-import './index.css';
-import App from './App.js';
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App.js";
 
-render(
-    <App />,
-  document.getElementById('root')
-);
-
+render(<App />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
